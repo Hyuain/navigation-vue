2adff45628c5e385b5b2e1f7d3ed1a8af088245d
Hyuain <Hyuain97@163.com>
2020-01-15 21:41:12 +0800

    增加长按功能

--- src/components/SiteList.vue
+++ src/components/SiteList.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="site-list">
-    <div class="site" v-for="site in sites" :key="site.url" @click="openSite(site)">
-      <div class="ico">
-        <img :src="site.ico" alt="ico" @error="handelIcoError($event)">
-        <span class="ico-success">{{site.textIco}}</span>
+    <div class="site" v-for="site in sites" :key="site.url">
+      <div class="content" v-longpress="showMobileDelete" @click="openSite(site)">
+        <div class="ico">
+          <img :src="site.ico" alt="ico" @error="handelIcoError($event)">
+          <span class="ico-success">{{site.textIco}}</span>
+        </div>
+        <div class="name">
+          {{site.name}}
+        </div>
       </div>
-      <div class="name">
-        {{site.name}}
+      <div class="mobile-delete-hide">
+        <div class="mobile-delete-inner" @click="deleteSite(site)">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
       </div>
+      <div class="pc-delete" @click="deleteSite(site)">
+        <svg class="icon">
+          <use xlink:href="#icon-close"></use>
+        </svg>
+      </div>
+
     </div>
     <div class="site add-button" @click="addSite">
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-add"></use>
-        </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-add"></use>
+      </svg>
     </div>
     <button @click="test">清空localstorage</button>
   </div>
@@ -20,6 +35,7 @@
 
 <script>
   import '../assets/icon'
+
   export default {
     name: "SiteList",
     inject: ['eventBus'],
@@ -39,19 +55,82 @@
             textIco: 'M'
           }
         ],
+        mobileDeleteShow: false
+      }
+    },
+    directives: {
+      longpress: {
+        bind: function (el, binding) {
+          let pressTimer = null
+          let longPress = false
+          const start = (e) => {
+            e.preventDefault()
+            if (e.type === 'click') {
+              return
+            }
+            if (pressTimer === null) {
+              pressTimer = setTimeout(() => {
+                handler(e)
+                longPress = true
+              }, 1000)
+            }
+          }
+          const cancel = () => {
+            if (pressTimer !== null) {
+              clearTimeout(pressTimer)
+              pressTimer = null
+            }
+          }
+          const handler = (e) => {
+            binding.value(e, el)
+          }
+          const bindEvents = (el) => {
+            el.addEventListener('touchstart', start)
+            el.addEventListener('touchend', (e) => {
+              cancel(e)
+              if (longPress === false) {
+                el.click()
+              }
+            })
+            document.addEventListener('click', () => {
+              el.nextSibling.classList.add('mobile-delete-hide')
+              el.nextSibling.classList.remove('mobile-delete-show')
+            })
+            el.addEventListener('touchcancel', cancel)
+          }
+          bindEvents(el)
+        }
       }
     },
     methods: {
+
+      deleteSite(site) {
+        this.sites.splice(this.sites.indexOf(site), 1)
+        localStorage.setItem('sites', JSON.stringify(this.sites))
+      },
+      showMobileDelete(e, el) {
+        this.vibration(20)
+        el.nextSibling.classList.remove('mobile-delete-hide')
+        el.nextSibling.classList.add('mobile-delete-show')
+      },
+      vibration(duration) {
+        window.navigator.vibrate = window.navigator.vibrate
+          || window.navigator.webkitVibrate
+          || window.navigator.mozVibrate
+          || window.navigator.msVibrate
+          || null
+        if (window.navigator.vibrate) {
+          window.navigator.vibrate(duration)
+        }
+      },
       addSite() {
         this.eventBus.$emit('openAddForm')
-        console.log('run')
       },
       test() {
         localStorage.clear()
         location.reload()
       },
       handelIcoError(event) {
-        console.log(event)
         event.target.classList.add('ico-error')
         event.target.nextSibling.classList.remove('ico-success')
         event.target.nextSibling.classList.add('ico-error')
@@ -79,64 +158,142 @@
     flex-wrap: wrap;
     margin-left: auto;
     margin-right: auto;
-    @media(min-width: 576px){
+    @media(min-width: 576px) {
       max-width: 460px;
     }
-    @media(min-width: 993px){
+    @media(min-width: 993px) {
       max-width: 900px;
     }
     .site {
       width: 100px;
       height: 100px;
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
       background-color: white;
       border-radius: 50%;
-      padding: 10px 0 10px;
-      position: relative;
-      cursor: pointer;
       box-shadow: 0 1px 1px #b9daff;
-      .ico{
-        width: 40px;
-        height: 40px;
+      position: relative;
+      .content {
+        width: 100%;
+        height: 100%;
         display: flex;
         justify-content: center;
+        flex-direction: column;
         align-items: center;
-        text-transform: uppercase;
-        font-size: 40px;
-        border-radius: 40%;
-        overflow: hidden;
-        img {
-          width: 120%;
-          height: 120%;
-          &.ico-error {
-            display: none;
+        padding: 10px 0 10px;
+        position: relative;
+        cursor: pointer;
+        .ico {
+          width: 40px;
+          height: 40px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-transform: uppercase;
+          font-size: 40px;
+          border-radius: 40%;
+          overflow: hidden;
+          img {
+            width: 120%;
+            height: 120%;
+            &.ico-error {
+              display: none;
+            }
+          }
+          span {
+            &.ico-error {
+              display: inline-block;
+            }
+            &.ico-success {
+              display: none;
+            }
           }
         }
-        span {
-
-          &.ico-error {
-            display: inline-block;
+        .name {
+          font-size: 14px;
+          margin-top: 7px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          width: 70px;
+          text-align: center;
+          vertical-align: center;
+        }
+      }
+      .pc-delete {
+        display: none;
+        @media (min-width: 576px) {
+          background-color: #ea4335;
+          position: absolute;
+          right: 0;
+          top: 0;
+          cursor: default;
+          width: 25px;
+          height: 25px;
+          border-radius: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          opacity: 0;
+          transition: all .3s;
+          .icon {
+            color: white;
+            width: 15px;
+            height: 15px;
           }
-          &.ico-success {
-            display: none;
+          &:hover {
+            transform: scale(1.1);
+            transition: transform .3s;
+            cursor: pointer;
+            .icon {
+              transform: rotateZ(180deg);
+              transition: transform .4s;
+            }
           }
         }
       }
-      .name{
-        font-size: 14px;
-        margin-top: 7px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        width: 70px;
-        text-align: center;
-        vertical-align: center;
+      &:hover {
+        .pc-delete {
+          opacity: 1;
+        }
       }
-      &.add-button{
-        .icon{
+      .mobile-delete-hide {
+        display: none;
+      }
+      .mobile-delete-show {
+        background-color: rgba(200, 200, 200, 0.8);
+        width: calc(100% + 2px);
+        height: calc(100% + 2px);
+        position: absolute;
+        top: -1px;
+        left: -1px;
+        border-radius: 4px;
+        .mobile-delete-inner {
+          background-color: rgba(150, 150, 150, 0.95);
+          width: 50px;
+          height: 50px;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateX(-50%) translateY(-50%);
+          border-radius: 50%;
+          .icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            font-size: 30px;
+            color: rgba(255, 255, 255, 0.9);
+          }
+        }
+      }
+      &.add-button {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px 0 10px;
+        position: relative;
+        cursor: pointer;
+        .icon {
           width: 50px;
           height: 50px;
           color: #1d78eb;
